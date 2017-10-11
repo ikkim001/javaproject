5694af3f2aab47a41b7b226e6132a0dcee84dce9
USER <USER@INET-15>
2017-10-11 19:00:49 +0900

    asdfadfsadfsafsasdfafasfdadsfadsfads

---
+++ Chapter06/src/Ex06_03.java
@@ -0,0 +1,22 @@
+
+public class Ex06_03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i;
+		
+		for (i = 0; i < 3; i++) {
+			System.out.print("Heloo?\n");
+			System.out.print("Re:Heloo?\n");
+		}
+		
+		System.out.print("\n\n");
+		
+		for (i = 0; i < 3; i++)
+			System.out.printf("Hello?\n");
+		System.out.printf("Re:Heloo?\n");
+
+	}
+
+}
---
+++ Chapter06/src/Ex06_04.java
@@ -0,0 +1,17 @@
+
+public class Ex06_04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 1~10까지의 합
+		
+		int hap=0; //누적
+		int i; // 반복
+		
+		// for(i=1;i<11;i++)
+		f
+		
+
+	}
+
+}
